24d1244f688254c7c9b07417128b1020f534b1fb
DanielRGilles <danryangilles@gmail.com>
2021-10-25 18:17:41 -0700

    added a test to make sure original array is not mutated using ...spread

--- __tests__/copyAndPush.test.js
+++ __tests__/copyAndPush.test.js
@@ -5,5 +5,9 @@ describe('push n that numbo', () => {
     const numbers = [1, 2, 3];
     expect(copyAndPush(numbers, 4)).toEqual([1, 2, 3, 4]);
   });
-  
+  it('checks to make sure the original array is mutated', () => {
+    const numbers = [1, 2, 3];
+    copyAndPush(numbers, 4);
+    expect(numbers).toEqual([1, 2, 3]);
+  });
 });
