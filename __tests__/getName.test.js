032b2b4dbc8f1144522424e816c9abdbe671cc40
DanielRGilles <danryangilles@gmail.com>
2021-10-25 17:12:59 -0700

    desctructured it all

--- __tests__/getName.test.js
+++ __tests__/getName.test.js
@@ -1,8 +1,12 @@
 const getName = require('../lib/utils/getName');
 
 describe('pull name out of an object', () => {
-  it('', () => {
-    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
-    expect(getName(spot)).toEqual('spot');
+  it('pulls name out of spot ', () => {
+    const { name } = { name: 'spot', age: 5, weight: '20 lbs' };
+    expect(getName({ name })).toEqual('spot');
+  });
+  it('pulls name out of character ', () => {
+    const { name } = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    expect(getName({ name })).toEqual('Aang');
   });
 });
